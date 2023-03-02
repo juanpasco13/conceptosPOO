768fb84d0f8a0cccc098b0badaa05c354fd9e291
Juan Pablo Garcia <jgarcia@tramisalud.com>
2023-03-02 11:12:02 -0500

    First Object

---
+++ conceptosPOO/conceptosPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace conceptosPOO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date = new Date(2023,2,4);
+            Console.WriteLine(date.ToString());
+        }
+    }
+}
---
+++ conceptosPOO/conceptosPOO/date.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace conceptosPOO
+{
+    public class Date
+    {
+        #region Fields
+        private int _year;
+        private int _month;
+        private int _day;
+        #endregion
+
+        #region Methods
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = ValidateMonth(month);
+            _day = day;
+        }
+
+        private int ValidateMonth(int month)
+        {
+            return (month > 0 && month < 13)?month : 0;
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            // Formas de imprimir un String de varios ITEMs
+            //return $"{_day}/{_month}/{_year}";
+            return String.Format("{0}/{1}/{2}", _day, _month, _year);
+        }
+        #endregion
+    }
+}
